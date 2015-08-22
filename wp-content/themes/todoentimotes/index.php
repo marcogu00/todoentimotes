acc803f815df305c60ad0343875b5fe3f949c472
marcogu00 <marcoskate74@gmail.com>
2015-08-22 07:13:11 -0430

    maps

--- wp-content/themes/todoentimotes/index.php
+++ wp-content/themes/todoentimotes/index.php
@@ -1,3 +1,31 @@
 <?php get_header(); ?>
+<?php $locator = CFS()->get('mapa_experiencia'); ?>
+
+		<script src="https://maps.googleapis.com/maps/api/js"></script>
+	    <script>
+	      function initialize() {
+	        var mapCanvas = document.getElementById('map-canvas');
+	        
+	        var myLatlng = new google.maps.LatLng(<?php echo $locator; ?>);
+	        
+	        var mapOptions = {
+	          center: myLatlng,
+	          zoom: 8,
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        }
+	        var map = new google.maps.Map(mapCanvas, mapOptions)
+	        
+	        var marker = new google.maps.Marker({
+			  position: myLatlng,
+			  map: map
+			});
+
+	      }
+	      google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
+			
+		<div id="map-canvas"></div>
+			
+	</div>
 
 <?php get_footer(); ?>
