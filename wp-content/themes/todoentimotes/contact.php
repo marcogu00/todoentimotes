cb99bf5e8cc7505162e7f3f5c801cc116104fb65
marcogu00 <marcoskate74@gmail.com>
2015-09-04 18:51:04 -0430

    contact

--- wp-content/themes/todoentimotes/contact.php
+++ wp-content/themes/todoentimotes/contact.php
@@ -4,8 +4,29 @@
 	<div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 text-center">
 		<h1><?php the_title(); ?></h1>
 		<p><?php the_content(); ?></p>
-		<form class="contact-form" action="" method="post">
-			<?php echo do_shortcode('[contact-form-7 id="137" title="Formulario de contacto 1"]'); ?>
+		<?php 
+		if(isset($_POST['enviar'])) { 
+			$headers = 'From:'.$_POST['nombre'].'<'.$_POST['correo'].'>';
+			$to="contacto@todoentimotes.com.ve";
+			$mensaje = $_POST['mensaje'];
+			$asunto="Contacto TodoEnTimotes.com.ve";	
+			mail( $to,$asunto,$mensaje, $headers );
+			echo "Mensaje enviado";
+		}
+			?>
+		<form class="contact-form" action="<?php bloginfo('url'); ?>/contacto/" method="post">
+			<p class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<input type="text" name="nombre" class="fom-control" placeholder="Nombre" required>
+			</p>
+			<p class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<input type="mail" name="correo" class="fom-control" placeholder="Email" required>
+			</p>
+			<p class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<textarea name="mensaje" id="" cols="30" rows="10" class="fom-control" placeholder="Mensaje"></textarea>
+			</p>
+			<p class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<input type="submit" class="fom-control" name="enviar" value="enviar">		
+			</p>
 		</form>
 		<ul class="redes_sociales text-center">
 			<?php 
@@ -25,3 +46,5 @@
 	</div>
 	<?php endwhile; ?>
 <?php get_footer(); ?>
+
+<div class="clearfix"></div>
